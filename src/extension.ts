094277acfd37e267e494b97e6212678eef7979ad
Gander7 <Gander7@users.noreply.github.com>
2022-04-28 21:06:15 -0300

    1 create todo (#2)
    * added toggle task
    * added keyboard shortcut alt+c
    * added removing and adding todos over multiple lines
    * reversed toggling multiple mixed lines to default add
    * added winston for logging later

--- src/extension.ts
+++ src/extension.ts
@@ -7,17 +7,111 @@ import * as vscode from "vscode";
 export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "todoer" is now active!');
+
+  const logger = vscode.window.createOutputChannel("Todoer");
+
+  // Would be nice to format lgoger at some point
+  // const winston = require('winston')
+  // https://stackoverflow.com/questions/34085330/how-to-write-to-log-from-vscode-extension
+  //   const logger = winston.Logger = winston.createLogger({
+  // 	  level: 'debug',
+  // 	  format: winston.format.combine(
+  // 		  winston.format.colorize(),
+  // 		  winston.format.simple(),
+  // 		  winston.format.timestamp({
+  // 			format: "YYYY-MM-DD HH:mm:ss"
+  // 		  }),
+  // 		  winston.format.printf((timestamp: string, level: string, message: string) =>
+  // 		  {
+  // 			  return `[${timestamp}] ${level} ${message}`
+  // 			})
+  // 	  ),
+  // 	  transports: [
+  // 		  new winston.transports.Console({ level: 'info' })
+  // 	  ]
+  //   })
+
+  // 	logger.info({ level: 'info', message: "Congrats, todoer is active!" })
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand("todoer.helloWorld", () => {
+  let disposable = vscode.commands.registerCommand("todoer.toggleTask", () => {
     // The code you place here will be executed every time your command is executed
     // Display a message box to the user
-    vscode.window.showInformationMessage("Hello World from Todoer!");
-  });
 
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      logger.appendLine("No Editor Found.");
+      return;
+    }
+    logger.appendLine("WE HAVE AN EDITOR");
+    const selection = editor.selection;
+    if (selection.isSingleLine) {
+      logger.appendLine("Detected: Single Line");
+
+      const { text } = editor.document.lineAt(selection.active.line);
+      if (text.startsWith("- [ ] ")) {
+        logger.appendLine("Detected: Existing Todo. Removing.");
+        const start = new vscode.Position(selection.active.line, 0);
+        const end = new vscode.Position(selection.active.line, 6);
+        editor.edit((edit) => edit.delete(new vscode.Range(start, end)));
+      } else {
+        logger.appendLine("Detected: No Existing Todo. Adding.");
+        editor.edit((edit) =>
+          edit.insert(new vscode.Position(selection.active.line, 0), "- [ ] ")
+        );
+      }
+    } else {
+      logger.appendLine("Detected: Multiple Lines");
+      const regex = /^- \[ \]/;
+
+      // TODO: This could be optimized using a multi line regex.
+      var hasAllTodos = true;
+      for (var i = selection.start.line; i <= selection.end.line; i = i + 1) {
+        var line = editor.document.lineAt(i).text;
+        if (!line.startsWith("- [ ] ")) {
+          hasAllTodos = false;
+          break;
+        }
+      }
+
+      if (hasAllTodos) {
+        // Remove
+        logger.appendLine("Detected: All Selected Lines are Todos. Removing.");
+        editor.edit((edit) => {
+          for (
+            var i = selection.start.line;
+            i <= selection.end.line;
+            i = i + 1
+          ) {
+            logger.appendLine("Removing Line: " + i.toString());
+            const start = new vscode.Position(i, 0);
+            const end = new vscode.Position(i, 6);
+            edit.delete(new vscode.Range(start, end));
+          }
+        });
+      } else {
+        // Add
+        logger.appendLine("Detected: Lines with no Todos. Adding.");
+        editor.edit((edit) => {
+          for (
+            var i = selection.start.line;
+            i <= selection.end.line;
+            i = i + 1
+          ) {
+            var line = editor.document.lineAt(i).text;
+            if (!line.startsWith("- [ ] ")) {
+              logger.appendLine("Adding Line: " + i.toString());
+              const line = new vscode.Position(i, 0);
+              edit.insert(line, "- [ ] ");
+            }
+          }
+        });
+      }
+    }
+    logger.appendLine("Success, added todo");
+  });
   context.subscriptions.push(disposable);
 }
 
