6bb791c00c1bb981ac1ce4150cbdea5d52882f56
J.M. Rah <plus.jamal@gmail.com>
2022-06-05 21:10:36 -0400

    Create unit tests for toggling tasks
    I started extracting core logic from the UI "layer" and separating it out
    so we could unit test it.
    
    #6

--- test/unit/placeholder.ts
+++
@@ -1,8 +0,0 @@
-import { expect } from "chai";
-
-describe("Hello World", () => {
-  it("Should be the correct greeting", () => {
-    const greeting = "Hello, World!";
-    expect(greeting).to.equal("Hello, World!");
-  });
-});
